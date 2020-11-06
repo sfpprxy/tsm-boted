e7b3a30ec94bb1764088aef1b911e4f15ed774de
Joe <rxyjoey@gmail.com>
2020-11-06 09:39:00 +0800

    add customized AuctionDB v1.01.04 and BetterVendorPrice v1.10.00

---
+++ BetterVendorPrice/CHANGELOG.md
@@ -0,0 +1,8 @@
+# Better Vendor Price
+
+## [v1.10.00](https://github.com/mooreatv/BetterVendorPrice/tree/v1.10.00) (2019-10-15)
+[Full Changelog](https://github.com/mooreatv/BetterVendorPrice/compare/v1.09.01...v1.10.00)
+
+- updated readme to mention config options  
+- new option to show only 1 line of info unless shift is held  
+    per request in curseforge comments  
---
+++ BetterVendorPrice/locale/koKR.lua
@@ -0,0 +1,14 @@
+-- Generated file, do not edit.
+local addon, _ns = ...
+
+if (GetLocale() ~= 'koKR') then return end
+
+local L = _G[addon].L
+L[" (current stack of %d)"] = "(현재 %d중첩)"
+L[" (full stack of %d)"] = "(최대 %d중첩)"
+L[" (item doesn't stack)"] = "(중첩 안 됨)"
+L[" (per item)"] = "(아이템 당)"
+L["Debug level"] = "디버그 레벨"
+L["Development, troubleshooting and advanced options:"] = "개발, 문제 해결 및 고급 옵션:"
+L["Vendors for:"] = "상점가:"
+
